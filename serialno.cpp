35c6846f191e00e107b814fecd48569efa6cd83b
kerimenes <kerimenesylmz@gmail.com>
2016-12-07 17:15:33 +0300

    SerianNoWidget: interface completed and sql added
    -Sql create table in your local serial.txt file
    -After comparison each othertable
    -Application development test completed...

--- serialno.cpp
+++ serialno.cpp
@@ -1,15 +1,22 @@
 #include "serialno.h"
 #include "ui_serialno.h"
 
-#include <QFileDialog>
 #include <QDebug>
 #include <QProcess>
+#include <QSqlQuery>
+#include <QFileDialog>
+#include <QSqlRelationalTableModel>
+#include <QTableView>
+#include <QSqlError>
+#include <QSqlRecord>
+#include <QSqlField>
 
 SerialNo::SerialNo(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::SerialNo)
 {
 	ui->setupUi(this);
+	ui->progressBar->setVisible(true);
 }
 
 SerialNo::~SerialNo()
@@ -80,22 +87,137 @@ QStringList SerialNo::listDir(const QString &desc)
 void SerialNo::searchInTxt(const QStringList ls)
 {
 	QStringList fileinside;
-	QStringList serialnolist;
-	for (int i = 0; i < ls.size(); i++) {
-		fileinside << fileOpen(QString("%1/%2").arg(ui->SearchPath->text()).arg(ls.at(i)));
-		if(fileinside.size() > 11) {
-			serialnolist << fileinside.at(11);
-			fileinside.clear();
-		}
-	}
 
-	for (int i = 0; i < serialnolist.size(); i++) {
-		QString s = serialnolist.at(i);
-		for (int j = 0; j < ui->pathOpen->count(); j++) {
-			if (s.contains(ui->pathOpen->item(j)->text()))
-			ui->pathOpen->item(j)->setForeground(Qt::green);
+	startProgress(ls.size());
+	for (int i = 0; i < ls.size(); i++) {
+		fileinside << fileOpen( QString("%1/%2").arg(ui->SearchPath->text()).arg(ls.at(i)) );
+		updateProgress(i, ls.size() - 1);
+		for (int k = 0; k < fileinside.size(); k++) {
+			for (int j = 0; j < ui->pathOpen->count(); j++) {
+				if ( fileinside.at(k).contains(ui->pathOpen->item(j)->text()) == true )
+					ui->pathOpen->item(j)->setForeground(Qt::green);
+			}
 		}
+		fileinside.clear();
 	}
 	qDebug() << "next";
+}
 
+void SerialNo::startProgress(int total)
+{
+	/* total value is ip numbers in encoderlist*/
+	ui->progressBar->setValue(0);
+	ui->progressBar->setMaximum(total);
+	ui->progressBar->setVisible(true);
+}
+
+void SerialNo::updateProgress(int progress, int total)
+{
+	ui->progressBar->setValue(progress);
+	if (total != -1)
+		ui->progressBar->setMaximum(total);
+	ui->progressBar->setVisible(true);
+}
+
+
+QSqlDatabase SerialNo::erpInit(const QString &dbServer, const QString &dbDatabase, const QString &dbUser, const QString &dbPass)
+{
+	QSqlDatabase db = QSqlDatabase::database();
+	if (db.isOpen())
+		qDebug() << "0";
+	db = QSqlDatabase::addDatabase("QPSQL");
+	db.setHostName(dbServer);
+	db.setDatabaseName(dbDatabase);
+	db.setUserName(dbUser);
+	db.setPassword(dbPass);
+	if (!db.open())
+		qDebug() << -EINVAL;
+	return db;
+
+}
+
+const QStringList SerialNo::getEncoderSerialAllList()
+{
+	QStringList encoderlist;
+
+	QSqlQuery q(QString("SELECT * FROM encoder_test_logging"));
+	while(q.next()) {
+		encoderlist << q.value("encoder_serial").toString();
+	}
+	return encoderlist;
+}
+
+void SerialNo::databaseClose(QSqlDatabase database)
+{
+	QString connection;
+	connection = database.connectionName();
+	database.close();
+	database = QSqlDatabase();
+	database.removeDatabase(connection);
+}
+
+void SerialNo::on_serialOpen_clicked()
+{
+	QSqlDatabase database = erpInit("maxwell.bilkon.arge", "BilkonErp", "BilkonErpAdmin", "Aibotah8");
+	getSerialNo();
+}
+
+void SerialNo::getSerialNo()
+{
+	QString pathexcel = QFileDialog::getOpenFileName(this, "Select Excel file ", "/home/kerim/myfs/");
+	ui->serialedit->setText(pathexcel);
+	QStringList listexcel = fileOpen(pathexcel);
+
+	startProgress(listexcel.size());
+	QSqlQuery q;
+	q.prepare(QString("Delete  from new_table"));
+
+	if(!q.exec()) {
+		ui->cmdOutput->setText(q.lastError().text());
+		qDebug() << q.lastError().text();
+	}
+
+	int i = 0;
+	foreach (QString serino, listexcel) {
+		i++;
+		updateProgress(i, listexcel.size());
+		createTempCalcTable(serino);
+	}
+}
+
+const QString SerialNo::createTempCalcTable(QString serino)
+{
+	QSqlQuery q;
+	q.prepare("INSERT INTO new_table( "
+			  "serial1) "
+			  "VALUES (?);");
+
+	q.addBindValue(serino);
+	if (!q.exec()) {
+		ui->cmdOutput->setText(q.lastError().text());
+		return q.lastError().text();
+	}
+	return "";
+}
+
+void SerialNo::checkSerialNumber()
+{
+	QSqlQuery q("SELECT serial1 FROM new_table "
+				"WHERE NOT EXISTS (SELECT * FROM encoder_test_logging "
+				"WHERE encoder_test_logging.encoder_serial = new_table.serial1)");
+	startProgress(q.numRowsAffected());
+	ui->pathOpen->clear();
+	int i = 0;
+	while(q.next()) {
+		ui->pathOpen->addItem(q.value("serial1").toString());
+		i++;
+		updateProgress(i, q.numRowsAffected());
+	}
+}
+
+void SerialNo::on_startSqlSearching_clicked()
+{
+	QSqlDatabase database = erpInit("maxwell.bilkon.arge", "BilkonErp", "BilkonErpAdmin", "Aibotah8");
+	ui->cmdOutput->setText(database.lastError().text());
+	checkSerialNumber();
 }
--- serialno.h
+++ serialno.h
@@ -2,6 +2,7 @@
 #define SERIALNO_H
 
 #include <QMainWindow>
+#include <QSqlDatabase>
 
 namespace Ui {
 class SerialNo;
@@ -18,12 +19,27 @@ public:
 	QStringList fileOpen(const QString &path);
 	QStringList listDir(const QString &desc);
 	void searchInTxt(const QStringList ls);
+	QSqlDatabase erpInit(const QString &dbServer, const QString &dbDatabase, const QString &dbUser, const QString &dbPass);
+	const QStringList getEncoderSerialAllList();
+	const QString createTempCalcTable(QString serino);
+	const QString copyTableToTable(const QString columnname, const QString oldtablename, const QString newtablename);
+	void checkSerialNumber();
+	void getSerialNo();
+	void cmdOutputEditor(const QString cmdoutput);
+protected:
+	void startProgress(int total);
+	void updateProgress(int progress, int total);
+	void databaseClose(QSqlDatabase database);
 private slots:
 	void on_browse_clicked();
 
 	void on_browseDir_clicked();
 
 	void on_startSearch_clicked();
+
+	void on_serialOpen_clicked();
+
+	void on_startSqlSearching_clicked();
 
 private:
 	Ui::SerialNo *ui;
